6a38358d6e7011835aa6d1558643a7efc93bbc76
Sam Thompson <samthompson292@gmail.com>
2020-12-03 20:45:09 +0000

    update and delete posts

--- myapp/depeche_house/routes.py
+++ myapp/depeche_house/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_login import login_required, login_user, logout_user, LoginManager, current_user
 from depeche_house.forms import LoginForm, RegistrationForm, UpdateProfileForm, AddPostForm
 from depeche_house.models import User, Post
@@ -143,9 +143,57 @@ def user():
 def add_post():
     post_form = AddPostForm()
     if post_form.validate_on_submit():
+        #inputting values to db table
         new_post = Post(title=post_form.title.data, content=post_form.text.data, author=current_user)
         db.session.add(new_post)
         db.session.commit()
         flash("Your post has been created!", 'success')
         return redirect(url_for('home'))
-    return render_template('add_post.html', post_form=post_form)
+    return render_template('add_post.html', post_form=post_form, legend="Add Post")
+
+
+
+@app.route('/post/<int:post_id>', methods=["GET", "POST"])
+@login_required
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', post=post)
+
+
+
+@app.route('/post/<int:post_id>/update', methods=["GET", "POST"])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    #if person clicking on link wasnt the author of the post then abort message
+    if post.author != current_user:
+        abort(403)
+    #creating a new instance of the post form 
+    update_form = AddPostForm()
+
+    if update_form.validate_on_submit():
+        post.title = update_form.title.data
+        post.content = update_form.text.data
+        db.session.commit()
+        flash("Post Updated", 'success')
+        return redirect(url_for('post', post_id=post.id))
+
+    elif request.method == "GET":
+        #makes post writing appear in the form inputs
+        update_form.title.data = post.title
+        update_form.text.data = post.content
+
+    return render_template('add_post.html', post_form=update_form, legend="Update Post")
+
+
+
+@app.route('/post/<int:post_id>/delete')
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your post has been deleted", 'success')
+    return redirect( url_for('home'))
