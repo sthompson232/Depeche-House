ccc016f6eee8692a6b39b2279097663f68b4c617
Sam Thompson <samthompson292@gmail.com>
2020-12-05 15:33:18 +0000

    pycache

--- myapp/routes.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from flask import request, render_template, flash, redirect,url_for
-from models import User
-
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-    return render_template('index.html')
