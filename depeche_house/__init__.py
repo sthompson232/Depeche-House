5a5deefd001cebf7ad88c9beae4ac670d5abc345
Sam Thompson <samthompson292@gmail.com>
2020-12-05 16:37:54 +0000

    heroku ready

--- depeche_house/__init__.py
+++ depeche_house/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '64c56f84624a5da4f7e8d386049ef370'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///depeche_house.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///depeche_house.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
