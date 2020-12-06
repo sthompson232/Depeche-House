a3bb90b5f051cfeda2a9f7e71f95e86837601570
Sam Thompson <samthompson292@gmail.com>
2020-12-06 18:44:27 +0000

    reinstalled VE with requirements and gunicorn

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Sam\Documents\GitHub\depeche _house\depeche_house
+set VIRTUAL_ENV=C:\Users\Sam\Documents\GitHub\depeche_house
 
 if not defined PROMPT set PROMPT=$P$G
 
