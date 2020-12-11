7e16c8a50d920b527f6eb8b57f401073348406ee
Sam Thompson <samthompson292@gmail.com>
2020-12-11 17:05:23 +0000

    changed profile pic string length in db

--- depeche_house/models.py
+++ depeche_house/models.py
@@ -18,7 +18,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), unique=True, index=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     joined_at = db.Column(db.DateTime(), default=datetime.utcnow, index=True, nullable=False)
-    profile_pic = db.Column(db.String(40), default='default.jpg')
+    profile_pic = db.Column(db.String(), default='default.jpg')
     posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
